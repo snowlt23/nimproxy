af73bf576a9955e61099a41a641c7004b713655c
snowlt23 <shsnow23@gmail.com>
2017-04-16 04:46:01 +0900

    fix regex of root path rewrite proc

--- nimproxy.nim
+++ nimproxy.nim
@@ -24,7 +24,7 @@ proc findRedirectPathFromConfig*(path: string): Option[string] =
   else:
     return none(string)
 
-let rootpathhtmlreg = re"""(src|href)\s*=\s*"\s*(/.*?)""""
+let rootpathhtmlreg = re"""(src|href|action)\s*=\s*"\s*(/.*?)""""
 let rootpathcssreg = re"""url\(\s*"*(/.*?)"*\)"""
 proc rewriteHTMLRootPath*(src: string, basepath: string): string =
   src.replace(rootpathhtmlreg, "$#=\"/$#$#\"" % ["$1", basepath, "$2"])
